af15531bac75eb7087849074a88d946b381c78ec
Robert Hill <Robertehill@users.noreply.github.com>
2019-01-28 07:04:35 -0800

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,25 @@ app.portPicstoHtml = function() {
     view.portPicstoHtml(value);
   });
 }
-$( document ).ready(function() {
+app.loadCar = function() {
+  $('.card').on('click', function() {
+    console.log(this.id);
+    $('.card').removeClass('full-screen');
+    $(this).addClass('full-screen');
+    // $('#portfolio-section').hide();
+    // $('#car'+this.id).addClass('active');
+  });
+}
+$(document).ready(function() {
     app.portPicstoHtml();
+    // app.loadCar();
+    view.fixedNavOnScroll();
 });
+// $(window).resize(function() {
+//   if ($(document).height() <= $(window).height()){
+//     $('.footer').addClass('footer-fixed');
+//   }
+//   else {
+//     $('.footer').removeClass('footer-fixed');
+//   }
+// }).resize()
--- assets/js/view.js
+++ assets/js/view.js
@@ -28,11 +28,26 @@ view.portPicstoHtml = function(obj) {
       bodyText = value;
     }
   });
-  var html = '<div class="card col-sm-10 col-md-4 col-lg-4 pad-1em margin-auto margin-bottom-1em"><img class="card-img-top" src="assets/img/portfolio/'+fileName+'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+picID+'</h5><div id="'+picID+'Body"></div></div><p class="text-bottom">Photo by '+photocredit+'</p></div>';
-  $('#portfolio-section').append(html);
-  // console.log("bodyText ="+ bodyText);
+  var cardHtml = '<div id="'+picID+'" class="card col-sm-10 col-md-4 col-lg-4 pad-1em margin-auto margin-bottom-1em"><img class="card-img-top" src="assets/img/portfolio/'+fileName+'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+picID+'</h5><div id="'+picID+'Body"></div></div><p class="text-bottom">Photo by '+photocredit+'</p></div>';
+  var carouselIndicators = '<li data-target="#carouselExampleIndicators" data-slide-to="'+(picID - 1)+'"></li>';
+  var portCarouselInner = '<div id="car'+picID+'" class="carousel-item"><img class="d-block margin-auto" src="assets/img/portfolio/'+fileName+'" alt=""></div>';
+  $('#portfolio-div').append(cardHtml);
+  $('#port-carousel-indicators').append(carouselIndicators);
+
+  $('#port-carousel-inner').append(portCarouselInner);
   $.each(bodyText, function( key, value ) {
     $('#'+picID+'Body').append('<p>'+value+'</p>');
     console.log(key+' '+value);
+  });
+}
+view.fixedNavOnScroll = function() {
+  $(document).scroll(function(e){
+      var scrollTop = $(document).scrollTop();
+      if(scrollTop >= $('header').height()+5){
+          // console.log(scrollTop);
+          $('.navbar').removeClass('navbar-static-top').addClass('fixed-top');
+      } else {
+          $('.navbar').removeClass('fixed-top').addClass('navbar-static-top');
+      }
   });
 }
