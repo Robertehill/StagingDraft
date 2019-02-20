0fc74dfb7496a99b51589c16232124e9620a8c28
Robert Hill <Robertehill@users.noreply.github.com>
2019-02-20 11:51:30 -0800

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,20 +10,9 @@ app.loadCar = function() {
     console.log(this.id);
     $('.card').removeClass('full-screen');
     $(this).addClass('full-screen');
-    // $('#portfolio-section').hide();
-    // $('#car'+this.id).addClass('active');
   });
 }
 $(document).ready(function() {
     app.portPicstoHtml();
-    // app.loadCar();
     view.fixedNavOnScroll();
 });
-// $(window).resize(function() {
-//   if ($(document).height() <= $(window).height()){
-//     $('.footer').addClass('footer-fixed');
-//   }
-//   else {
-//     $('.footer').removeClass('footer-fixed');
-//   }
-// }).resize()
--- assets/js/model.js
+++ assets/js/model.js
@@ -3,8 +3,8 @@ var portfolioPicsList = [
     "picID": "1",
     "houseID" :"1",
     "fileName":"KMQ_1002-thumb.jpg",
-    "titletext":"Regal Newcastle Rambler $800,000",
-    "bodyText": ["This newly remodeled home motivated us to choose ornate furnishings, glass, gold, silver, leather and tufted nail head chairs gave it a sophisticated feel. We purposefully did not add a rug in the living room so that buyers could see the gorgeous new floors.", "Our color palette was inspired by the soft grey/blue walls and grey flooring, adding the over sized canvas in the dining room brought the buyers attention to the entire large space."],
+    "titletext":"Regal Newcastle Rambler",
+    "bodyText": ["This newly remodeled home motivated us to choose ornate furnishings, glass, gold, silver, leather and tufted nail head chairs to give it a sophisticated feel. We purposefully did not add a rug in the living room so that buyers could see the gorgeous new floors.", "Our color palette was inspired by the exterior color and the soft grey/blue walls and grey flooring inside. Adding the over-sized canvas in the dining room brought the buyers attention to the entire large space."],
     "photocredit":"Keith Morgan",
     "photoCredLink":"https://www.kmasonphoto.com"
   },
@@ -13,14 +13,14 @@ var portfolioPicsList = [
     "houseID" :"1",
     "fileName":"Bonus_rm-thumb.jpg",
     "titletext":"Game room in Lake Stevens $695,000 SOLD!",
-    "bodyText": ["this room was huge, so staging this room with &#34;zones&#34; allowed the buyer to see how this room can have many purposes. A relaxing movie area, pool table game area, plus the back room we staged as a guest bedroom. We used red accents, its a great color to use in an active room such as this one, it attracts the buyers eye to specific focal points that we want them to see.", ""],
+    "bodyText": ["Because this room was so massive, we staged it into &#34;use-areas&#34;, sectioning off spaces to create imagination for the purpose of the room.  A relaxing movie area, pool table game area, plus the back room we staged as a guest bedroom. We used red accents, as it is a great color to grab the buyers attention.", ""],
     "photocredit":"Ivy Jo Houghton, Ivy League Staging"
   },
   {
     "picID": "3",
     "houseID" :"1",
     "fileName":"Bonus_room_bedrm.jpg",
-    "titletext":"Bicycle bedroom Lake Stevens $695,000 ",
+    "titletext":"Bicycle bedroom Lake Stevens $695,000",
     "bodyText": ["This bedroom is just off the bonus room, so we choose a theme to match. Smaller rooms like this one require strategic styling using bold colors in order to take the buyers attention away form the size of the room. The faux suede red quilt gives the room soft texture.", ""],
     "photocredit":"Photos by Tifiny",
     "photoCredLink":"https://www.facebook.com/PhotosByTifiny"
@@ -29,8 +29,8 @@ var portfolioPicsList = [
     "picID": "4",
     "houseID" :"1",
     "fileName":"GlassOffice.jpg",
-    "titletext":"Glass Office",
-    "bodyText": ["The seller requested the 4th bedroom be staged as an office as it was the smallest of the bedrooms, we used a glass desk, and monochromatic black, white and grey. To allow the room to appear larger.", ""],
+    "titletext":"Glass office in Newcastle",
+    "bodyText": ["Often times a 4th bedroom is staged as an office as it's typically the smallest of the bedrooms, we used a glass desk, and monochromatic black, white and grey. To allow the room to appear larger.", ""],
     "photocredit":"Keith Morgan",
     "photoCredLink":"https://www.kmasonphoto.com"
   },
@@ -38,32 +38,32 @@ var portfolioPicsList = [
     "picID": "5",
     "houseID" :"1",
     "fileName":"LivingRoomMC.jpg",
-    "titletext":"Living Room Mukilteo $690,000",
-    "bodyText": ["The sellers were living here so we assisted them in the staging. By de-cluttering some rooms, rearranging the furnishings and adding more we made the most of the floor plan. If you need staging in a lived in home, or just a consultation of how to rearrange the furnishings, we do that as well!", ""],
+    "titletext":"Formal living room in Mukilteo",
+    "bodyText": ["The sellers occupied this home while it was on the market. We removed clutter, rearranged the furnishings and added specific decor to reflect the traditional style of this 2-story home,  If you need staging in a lived-in home, or just a consultation of how to rearrange the furnishings, we do that as well!", ""],
     "photocredit":"Ivy Jo Houghton, Ivy League Staging"
   },
   {
     "picID": "6",
     "houseID" :"1",
     "fileName":"MediaRoom.jpg",
-    "titletext":"Basement / Media Game Room",
-    "bodyText": ["This Seattle home had a low ceiling basement and the room was an awkward shape. We staged it in &#34;zones&#34;, a board game table area, bar, foosball table and the TV gaming area. It sold quick and for full price!", ""],
+    "titletext":"Basement bonus room in Wedgewood",
+    "bodyText": ["This Seattle home had low ceilings in the basement and this room was an awkward shape so we staged it in &#34;use-areas&#34; this way a buyer could see it's maximum potential. We made it fun using a board game table and bar, Foosball, and a TV gaming area.", ""],
     "photocredit":"Ivy Jo Houghton, Ivy League Staging"
   },
   {
     "picID": "7",
     "houseID" :"1",
     "fileName":"LivSeattle.jpg",
-    "titletext":"Seattle Remodeled home",
-    "bodyText": ["When you have a narrow living room, use the fireplace as a focal point. We thought matching couches facing each other would be an ideal way to show the living room had plenty of space for seating.", ""],
+    "titletext":"Quaint home in Seattle",
+    "bodyText": ["This narrow living room had an enticing fireplace to showcase. We thought matching couches facing each other would be an ideal way to show off the mantel, as well as allow the buyers to see it had plenty of space for seating.", ""],
     "photocredit":"William Russell"
   },
   {
     "picID": "8",
     "houseID" :"1",
     "fileName":"KMQ_0712-thumb.jpg",
-    "titletext":"Cle Elum 5 acres, Huge Shop",
-    "bodyText": ["We knew this house would attract the outdoorsy type, so this bedroom got our &#34;Wilderness&#34; style. Using neutral colors and very specific bedding, rustic night stands, ceramic &#34;rock&#34; looking lamps and landscape canvases. Sure enough the buyers loved it!", ""],
+    "titletext":"Guest bedroom in Cle Elum",
+    "bodyText": ["We knew this house would attract the outdoorsy type, so this bedroom got a &#34;wilderness&#34; design. Using neutral colors and very specific bedding, rustic night stands, ceramic &#34rock&#34 looking lamps and landscape canvases. Sure enough the buyers loved it!", ""],
     "photocredit":"Keith Morgan",
     "photoCredLink":"https://www.kmasonphoto.com"
   },
@@ -71,17 +71,17 @@ var portfolioPicsList = [
     "picID": "9",
     "houseID" :"1",
     "fileName":"bluebath-thumb.jpg",
-    "titletext":"Green and Blue Bathroom",
-    "bodyText": ["Now and then we run across a bathroom that has outdated fixtures, most stagers will use items that detract from them. We use decorator touches that compliment it, might as well show the new buyer how to decorate it.", ""],
+    "titletext":"Mint green bathroom in Cle Elum",
+    "bodyText": ["Now and then we run across a bathroom that has bold fixtures, most stagers will use decor to avoid bringing attention to them. We prefer to compliment them with matching decorator touches; shedding a new light on what may be considered &#34;outdated&#34;.", ""],
     "photocredit":"Keith Morgan",
     "photoCredLink":"https://www.kmasonphoto.com"
   },
   {
     "picID": "10",
     "houseID" :"1",
-    "fileName":"Bath.jpg",
-    "titletext":"Pink Bathroom",
-    "bodyText": ["Here is another example of what we do to overcome the objection of an outdated bathroom. We compliment the color with the art work and decor. The buyer plans to keep the pink fixtures so the house has a retro feel.", ""],
+    "fileName":"Masterbedfallcity.jpg",
+    "titletext":"Master Bedroom in Issaquah",
+    "bodyText": ["Often times we run across enormous master bedrooms with an abundance of floor space. In this master we used an elegant settee next to the fireplace to create a relaxing ambiance, making the room feel more like a luxurious hotel suite.", ""],
     "photocredit":"Brandon Temple",
     "photoCredLink":"http://bellevuehomephotographer.com"
   },
@@ -89,17 +89,25 @@ var portfolioPicsList = [
     "picID": "11",
     "houseID" :"1",
     "fileName":"KM100847.jpg",
-    "titletext":"Lake Stevens $225,000",
-    "bodyText": ["Every house on the market deserves to be shown in it's best light, even the less expensive ones.This house was lacking counter space, so with the sellers permission, we purchased an island and small cart with baskets to complete this kitchen.", ""],
+    "titletext":"Kitchen in Lake Stevens",
+    "bodyText": ["Sometimes we come across a house that requires a permanent furniture for a solution to a problem, in this case it was a lack of usable counter space. With the sellers permission, we purchased an island and small cart with baskets to complete this kitchen.", ""],
     "photocredit":"Keith Morgan",
     "photoCredLink":"https://www.kmasonphoto.com"
   },
   {
     "picID": "12",
     "houseID" :"1",
-    "fileName":"20171011_133739.jpg",
-    "titletext":"Lake Stevens $206,000",
-    "bodyText": ["Another example of how we can make even the most sparse of rooms, feel warm and inviting.", ""],
+    "fileName":"NauticalRoom.jpg",
+    "titletext":"Children's room in Renton",
+    "bodyText": ["Buyers often have their children with them when they look at homes, therefore staging some of the bedrooms to cater to the younger audiences can be an effective marketing technique. This can get the kids excited about living there which can sway the parents buying decision.", ""],
+    "photocredit":"Ivy Jo Houghton, Ivy League Staging"
+  },
+  {
+    "picID": "13",
+    "houseID" :"1",
+    "fileName":"RedBycicleBedroom.jpg",
+    "titletext":"Children's room in Renton",
+    "bodyText": ["Smaller rooms like this one require strategic styling using bold colors in order to take the buyers attention away from the size of the room. The faux suede red quilt gives the room soft texture, and then we added a theme of bicycles just for fun!", ""],
     "photocredit":"Ivy Jo Houghton, Ivy League Staging"
   },
 ];
